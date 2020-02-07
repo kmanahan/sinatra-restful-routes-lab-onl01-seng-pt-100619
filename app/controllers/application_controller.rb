65f9acae23b38d711cade53bdd27e3feb89f4ee0
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-02-07 19:51:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do 
+    @recipes = Recipe.all
+    erb :index
+  end 
 
+  get '/recipes/new' do 
+    erb :index
+  end 
+  
+  post '/recipes/new' do 
+    @recipes = Recipe.all(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time]) 
+    @recipes.save 
+  end 
 end
